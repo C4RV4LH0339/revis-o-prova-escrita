8e035377f037111f840c9053c434304ffecc3899
C4RV4LH0339 <dscarvalho987@gmail.com>
2026-04-14 09:09:58 -0300

    teste

--- script.js
+++ script.js
@@ -1,24 +1,9 @@
 function alterarNome() {
-    let trocarNome = document.getElementById("inputNome").value; 
+    let trocarNome = document.getElementById('inputNome').value;
+
     if (trocarNome === "") {
-        alert("Digite um nome")
+        alert('Digite um Nome')
     } else {
-        document.getElementById("nomeUsuario").innerHTML = trocarNome;
-    }
-}
-function alterarCurso() {
-    let trocarCurso = document.getElementById("inputCurso").value; 
-    if (trocarCurso === "") {
-        alert("Digite um Curso")
-    } else {
-        document.getElementById("nomeCurso").innerHTML = trocarCurso;
-    }
-}
-function alterarStatus() {
-    let trocarStatus = document.getElementById("inputStatus").value; 
-    if (trocarStatus === "") {
-        alert("Digite um Status")
-    } else {
-        document.getElementById("nomeStatus").innerHTML = trocarStatus;
+        document.getElementById('nomeUsuario').innerHTML = trocarNome;
     }
 }
